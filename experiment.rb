07fcd7ebd297d2f08b803758b950069ab463a3f9
Sarah Tatro <statrox4@gmail.com>
2019-06-10 15:02:17 -0600

    Add unique http response for debugging

--- experiment.rb
+++ experiment.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 require 'socket'
 tcp_server = TCPServer.new(9292)
 
@@ -13,9 +15,23 @@ while true
 
   puts "Got this request:"
   puts request_lines.inspect
+  split_lines = request_lines.map { |req_line| req_line.split(":")}
+  split_header_on_space = split_lines[0].map { |path| path.split(" ") }
 
   puts "Sending response."
-  response = "<pre>" + request_lines.join("\n") + "</pre>"
+  # binding.pry
+  response_arr = [
+    "Verb": split_header_on_space[0][0],
+    "Path": split_header_on_space[0][1],
+    "Protocol": split_header_on_space[0][2],
+    "Host": split_lines[1][1],
+    "Port": split_lines[1][2],
+    "Origin": split_lines[1][1],
+    "Accept": split_lines[6][1]
+  ]
+
+  # response = "<pre>" + request_lines.join("\n") + "</pre>"
+  response = "<pre>#{response_arr}</pre>"
 
   greeting = "hello world! + #{count}"
 
