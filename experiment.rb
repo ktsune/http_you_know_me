18bb69d1bfee5ada1c3a3684c71d3702103a5fce
Sarah Tatro <statrox4@gmail.com>
2019-06-11 13:40:19 -0600

    Fix functionality to output diagnostics

--- experiment.rb
+++ experiment.rb
@@ -1,4 +1,6 @@
 require 'pry'
+require 'faraday'
+require 'faraday_middleware'
 
 require 'socket'
 tcp_server = TCPServer.new(9292)
@@ -15,27 +17,33 @@ while true
 
   puts "Got this request:"
   puts request_lines.inspect
-  split_lines = request_lines.map { |req_line| req_line.split(":")}
-  split_header_on_space = split_lines[0].map { |path| path.split(" ") }
+  # binding.pry
+  response_arr = {}
+  request_lines.map do |req_line|
+    split = req_line.split(": ")
+    if req_line.include? "HTTP"
+      split = req_line.split(" ")
+      response_arr["Verb"] = split[0]
+      response_arr["Path"] = split[1]
+      response_arr["Protocol"] = split[2]
+    end
+      response_arr[split[0]] = split[1]
+  end
 
   puts "Sending response."
-  # binding.pry
-  response_arr = [
-    "Verb": split_header_on_space[0][0],
-    "Path": split_header_on_space[0][1],
-    "Protocol": split_header_on_space[0][2],
-    "Host": split_lines[1][1],
-    "Port": split_lines[1][2],
-    "Origin": split_lines[1][1],
-    "Accept": split_lines[6][1]
-  ]
 
-  # response = "<pre>" + request_lines.join("\n") + "</pre>"
-  response = "<pre>#{response_arr}</pre>"
+  response_arr = {
+    "Verb": response_arr["Verb"],
+    "Path": response_arr["Path"],
+    "Protocol": response_arr["Protocol"],
+    "Host": response_arr["Host"].split(":")[0],
+    "Port": response_arr["Host"].split(":")[1],
+    "Origin": response_arr["Host"].split(":")[0],
+    "Accept": response_arr["Accept"]
+  }
 
   greeting = "hello world! + #{count}"
-
-  output = "<html><head></head><body>#{response}#{greeting}</body></html>"
+  output = "<html><head></head><body>#{response_arr}#{greeting}</body></html>"
   headers = ["http/1.1 200 ok",
             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
             "server: ruby",
