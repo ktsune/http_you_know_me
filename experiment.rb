c96f5acc9c7797997bb1562f22020b9b4eaba470
Sarah Tatro <statrox4@gmail.com>
2019-06-12 19:53:23 -0600

    Add supporting params

--- experiment.rb
+++ experiment.rb
@@ -19,7 +19,6 @@ while running
 
   puts "Got this request:"
   puts request_lines.inspect
-  # binding.pry
   response_arr = {}
   response_arr[:Host] = 'localhost:9292'
   request_lines.map do |req_line|
@@ -28,6 +27,11 @@ while running
       split = req_line.split(" ")
       response_arr[:Verb] = split[0]
       response_arr[:Path] = split[1]
+      if split[1].include?'?'
+        split_on_question = split[1].split('?')
+        response_arr[:Path] = split_on_question[0]
+        response_arr[:Params] = split_on_question[1]
+      end
       response_arr[:Protocol] = split[2]
     else
       response_arr[split[0]] = split[1]
@@ -36,7 +40,7 @@ while running
 
   puts "Sending response."
 
-  response_arr = {
+  supporting_paths = {
     "Verb": response_arr[:Verb],
     "Path": response_arr[:Path],
     "Protocol": response_arr[:Protocol],
@@ -48,13 +52,26 @@ while running
 
   puts "Composed response array."
 
-
-  case response_arr[:Path]
+  case supporting_paths[:Path]
   when '/hello'
     greeting = "hello world! + #{count}"
-    output = "<html><head></head><body>#{response_arr}#{greeting}</body></html>"
+    output = "<html><head></head><body>#{supporting_paths}#{greeting}</body></html>"
   when '/datetime'
     output = Date.today.strftime("%I:%M on %A, %B %d, %Y")
+  when '/word_search'
+
+    split_on_equals = response_arr[:Params].split("=")
+    word = split_on_equals[1]
+
+    path = IO.readlines('/usr/share/dict/words')
+
+    output = "<html><head></head><body>WORD is not a known word</body></html>"
+    path.each do |word_in_words|
+      if word == word_in_words.rstrip
+        output = "<html><head></head><body>WORD is a known word</body></html>"
+      end
+    end
+
   when '/shutdown'
     output = "total requests: + #{count}"
     running = false
